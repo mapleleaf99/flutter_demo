a3669e097a5cef9de47bb197694bbe86a5775326
GFF <mapleleaf99@126.com>
2024-07-03 21:48:02 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "APP",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("我的标题"),
+        ),
+        body: ContentPage(),
+      ),
+    );
+  }
+}
+
+class ContentPage extends StatefulWidget {
+  const ContentPage({super.key});
+
+  @override
+  State<ContentPage> createState() => _ContentPageState();
+}
+
+class _ContentPageState extends State<ContentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("文本");
+  }
+}
+
+
